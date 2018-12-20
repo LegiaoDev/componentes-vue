dd2749f901b5749cb1ba6fdfb6efd2d22b9c3af8
Gabriel <gabrielantoniocp9@gmail.com>
2018-12-20 00:06:25 -0200

    ïntegracao com bulma e buefy e criacao do primeiro componente

---
+++ src/views/Menus.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="menu">
+    <MenuUm/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MenuUm from '@/components/menu/MenuUm.vue';
+
+export default {
+  name: 'menu',
+  components: {
+    MenuUm,
+  },
+};
+</script>
